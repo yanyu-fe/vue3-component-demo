62937984015ac90ab5dd8c66c318ef21fa920ff3
aibayanyu <aibayanyu@qq.com>
2022-04-27 21:01:59 +0800

    feat: add button type

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -1,6 +1,13 @@
+import type { PropType } from 'vue'
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'YButton',
+  props: {
+    type: {
+      type: String as PropType<'default'|'primary'>,
+      default: 'default',
+    },
+  },
   setup(_props, { slots }) {
     return () => {
       return <button>{ slots.default && slots.default() }</button>
